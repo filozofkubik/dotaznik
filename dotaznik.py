ebe241154e10bf15e54023fdcbf640e1f813bf59
jakub Šantora <jakub.santora.g@gmail.com>
2024-10-26 20:13:18 +0200

    Založení programu

---
+++ dotaznik.py
@@ -0,0 +1,25 @@
+import turtle
+import random
+turtle.Screen().colormode(255)
+pencil = turtle.Turtle()
+pencil.shape("circle")
+text =["pravice", "libertariánství", "levice", "autoritářství"]
+for i in range (4):
+    pencil.forward(400)
+    pencil.write(text[i])
+    pencil.backward(400)
+    pencil.right(90)
+
+turtle.done()
+
+answer = int(input("kterou odpov2d?????"))
+q1 = []
+q2 = []
+q3 = []
+q4 = []
+q5 = []
+q6 = []
+q7 = []
+q8 = []
+q9 = []
+q10 = []
